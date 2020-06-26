0db40ffed64b24efb9f7c151372523c4d4674e47
FOXFOXYS <foxfoxy.pro@gmail.com>
2020-06-26 14:49:41 +0200

    Ajout de .php indespensable au fonctionnement + modification

--- header.php
+++ header.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html <?php language_attributes(); ?>>
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Effet Bois</title>
+
     <?php wp_head(); ?>
 </head>
-<body>
-    <? wp_nav_menu('primary'); ?>
+<body <?php body_class(); ?>>
+    <header class="header">
+        <a href="<?php echo home_url( '/' ); ?>">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/logos.png" alt="Logo">
+        </a>  
+  </header>
