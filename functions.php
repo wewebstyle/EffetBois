1d7d1c52ce6165be986c52e99cfca97348e92f9d
FOXFOXYS <foxfoxy.pro@gmail.com>
2020-06-26 15:18:39 +0200

    Modification fichier php + ajout

--- functions.php
+++ functions.php
@@ -16,3 +16,17 @@ add_theme_support( 'post-thumbnails' );
 
 // Ajouter automatiquement le titre du site dans l'en-tête du site
 add_theme_support( 'title-tag' );
+
+// add Menus
+register_nav_menus( array(
+	'main' => 'Menu Principal',
+	'footer' => 'Bas de page',
+) );
+
+// Déclaration CSS 
+wp_enqueue_style( 
+	'capitaine', 
+	get_template_directory_uri() . '/css/main.css',
+	array(), 
+	'1.0'
+);
--- header.php
+++ header.php
@@ -3,12 +3,14 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <link rel="stylesheet" href="style.css">
     <?php wp_head(); ?>
 </head>
-<body <?php body_class(); ?>>
+<body>
     <header class="header">
         <a href="<?php echo home_url( '/' ); ?>">
             <img src="<?php echo get_template_directory_uri(); ?>/img/logos.png" alt="Logo">
         </a>  
+
+        <?php wp_nav_menu( array( 'theme_location' => 'main' ) ); ?>
   </header>
